5b08012a8100eb1c0f6f65236c95354d4509edf5
Bjørn Røgeberg <bjorn.rogeberg@basefarm.com>
2024-05-23 13:19:20 +0200

    PCE-89 Align default log retention with company policy

--- modules/delegated_account/variables.tf
+++ modules/delegated_account/variables.tf
@@ -105,7 +105,7 @@ variable "log_group_retention" {
   Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653, and 0
   If you select 0, the events in the log group are always retained and never expire.
   EOT
-  default     = 60
+  default     = 731
 }
 
 variable "reserved_concurrent_executions" {
